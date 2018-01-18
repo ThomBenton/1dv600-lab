3a04f2a355b15c65e93d8ac1f4cf397d0b4136e0
ThomBenton <mrresident@hotmail.com>
2018-01-18 18:35:00 +0100

    first commit

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -1,11 +1,9 @@
 (function () {
-    "use strict";
+  'use strict'
 
-    var LibraryDAO = require('../dao/LibraryDAO');
+  var LibraryDAO = require('../dao/LibraryDAO')
 
+  module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
 
-    module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-
-    };
-
-}());
+  }
+}())
